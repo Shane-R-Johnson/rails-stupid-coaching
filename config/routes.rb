3fa0409fa8178542acfb9e21ac8d325894daabed
Shane Johnson <sj34606@gmail.com>
2019-11-11 16:39:58 +0000

    stupider coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# It should look like this:
+
+#   Prefix Verb URI Pattern       Controller#Action
+#      ask GET  /ask(.:format)    questions#ask
+#   answer GET  /answer(.:format) questions#answer
+get 'answer', to: "questions#answer"
+get 'ask', to: "questions#ask"
+
 end
